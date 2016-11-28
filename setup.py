1cfadf37e496a210affba657919ceabba5b7dc88
Dan Riti <dmriti@gmail.com>
2016-11-28 15:54:17 -0500

    Update package for source distribution.

--- setup.py
+++ setup.py
@@ -58,7 +58,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
 
-from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
 import shutil
 import sys
 
@@ -78,16 +81,19 @@ if sys.platform == "win32":
     libs.append("ws2_32")
 
 
-module1 = Extension('amysql',
-                sources = ['amysql.c', 'io_gevent.c', 'capi.cpp', 'Connection.cpp', 'PacketReader.cpp', 'PacketWriter.cpp', 'SHA1.cpp'],
-                include_dirs = ['./'],
-                library_dirs = [],
-                libraries=libs,
-                define_macros=[('WIN32_LEAN_AND_MEAN', None)])
+module1 = Extension(
+    'amysql',
+    sources=['amysql.c', 'io_gevent.c', 'capi.cpp', 'Connection.cpp', 'PacketReader.cpp', 'PacketWriter.cpp', 'SHA1.cpp'],
+    include_dirs=['./'],
+    library_dirs=[],
+    libraries=libs,
+    define_macros=[('WIN32_LEAN_AND_MEAN', None)]
+)
 
-setup (name = 'amysql',
-       version = '1.0',
-       description = 'Ultra fast MySQL driver for Python',
-       ext_modules = [module1])
-
-
+setup(name='amysql',
+      version='1.0.0tly',
+      description='Ultra fast MySQL driver for Python',
+      url='https://github.com/tracelytics/amysql',
+      author='tracelytics',
+      author_email='ops@tracelytics.com',
+      ext_modules=[module1])
